2e790ab16f7945efbc47de78f7ef0e87159e368d
Gabe <solidarity@webmail.hu>
2021-08-31 14:44:32 +0200

    2021.08.31.

---
+++ js/buborek.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+  
+  
+  // SÖVEGBUBORÉKOK
+  // Cukrászműhely
+  
+  $('#szoveg1').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'right',
+      y: 'top'
+    },
+	  width: 270
+});
+  
+  $('#szoveg2').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'left',
+      y: 'top'
+    },
+	  width: 260
+});
+
+  $('#szoveg3').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'center',
+      y: 'top'
+    },
+	  width: 260
+});
+
+  $('#szoveg4').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'center',
+      y: 'top'
+    },
+	  width: 260
+});
+  $('#szoveg5').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'center',
+      y: 'top'
+    },
+	  width: 260
+});  
+  $('#szoveg6').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'right',
+      y: 'top'
+    },
+	  width: 185
+});  
+  $('#szoveg7').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'right',
+      y: 'top'
+    },
+	  width: 285
+});  
+  $('#szoveg8').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'right',
+      y: 'top'
+    },
+	  outside: 'x',
+	  offset: {x: -25, y: -30},
+	  width: 200
+}); 
+  
+  
+  // Gondolat
+  
+  $('#szoveg9').jBox('Tooltip', {
+  attach: '.tooltip',
+  trigger: 'mouseenter',
+	  position: {
+      x: 'right',
+      y: 'top'
+    },
+	  width: 300,
+	  addClass: 'thought'
+});
+  
+
+});
