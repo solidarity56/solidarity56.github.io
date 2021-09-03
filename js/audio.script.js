12a121ec491efa67428be719aa29300c87e1a8fe
Gabe <solidarity@webmail.hu>
2021-09-03 14:26:40 +0200

    2021.09.03.

---
+++ js/audio.script.js
@@ -0,0 +1,14 @@
+//Defining variable based on unique ID
+
+var audio1 = document.getElementById("audioID");
+var audio2 = document.getElementById("audioID2");
+
+//Example of an HTML Audio/Video Method
+
+function playAudio1() {
+  audio1.play();
+}
+
+function playAudio2() {
+  audio2.play();
+}
