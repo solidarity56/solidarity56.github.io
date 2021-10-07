679dfa26bd9b142835b8c1ea0eaebd4442ada1e6
Gabe <solidarity@webmail.hu>
2021-10-07 13:47:09 +0200

    sound

--- js/audio.script.js
+++ js/audio.script.js
@@ -6,11 +6,11 @@ var audio2 = document.getElementById("audioID2");
 //Example of an HTML Audio/Video Method
 
 function playAudio1() {
-  audio1.volume = 0.1;
+  audio1.volume = 0.3;
   audio1.play();
 }
 
 function playAudio2() {
-  audio2.volume = 0.1;
+  audio2.volume = 0.3;
   audio2.play();
 }
