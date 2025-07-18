bbaf71375a3c4d93c9e5b32b5fdf7bab1b731997
Dworschák Gábor <solidarity@webmail.hu>
2025-07-18 13:33:40 +0200

    new

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ async function scrapeAndSave() {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://példa-oldalad.github.io', { waitUntil: 'networkidle0' });
+    await page.goto('https://solidírity56.github.io', { waitUntil: 'networkidle0' });
 
     const selector = "#__nuxt > div > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div.text-h5.text-md-h2.text-archivo-expanded.mb-2.d-flex.justify-space-between > p:nth-child(1)";
     const number = await page.$eval(selector, el => el.innerText.trim());
